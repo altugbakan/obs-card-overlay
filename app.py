566aa3674f4d5017bcd6b47a65b850af9161b39d
Altuğ Bakan <mail@alt.ug>
2025-11-18 11:44:05 +0100

    change port

--- app.py
+++ app.py
@@ -3,18 +3,23 @@ from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
+port = 5050
+
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/control")
 def control():
     return render_template("control.html")
 
+
 @app.route("/overlay")
 def overlay():
     return render_template("overlay.html")
+
 
 @socketio.on("set_card")
 def handle_set_card(data):
@@ -24,6 +29,7 @@ def handle_set_card(data):
         emit("show_card", {"name": name, "url": url}, broadcast=True)
         print(f"Broadcasted card: {name} -> {url}")
 
+
 if __name__ == "__main__":
-    print("Starting Flask + SocketIO server on port http://localhost:5000")
-    socketio.run(app, host="0.0.0.0", port=5000)
+    print(f"Starting Flask + SocketIO server on port http://localhost:{port}")
+    socketio.run(app, host="0.0.0.0", port=port)
