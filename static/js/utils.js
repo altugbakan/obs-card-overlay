163bedd9e340823d752e07818e82e25a16705d51
Altuğ Bakan <mail@alt.ug>
2025-11-18 19:24:42 +0100

    fix socket port

--- static/js/utils.js
+++ static/js/utils.js
@@ -1,4 +1,4 @@
-export const socket = io("http://127.0.0.1:5000");
+export const socket = io("http://127.0.0.1:5050");
 export function showToast(msg, type = "error") {
     const container = document.getElementById("toast-container");
 
