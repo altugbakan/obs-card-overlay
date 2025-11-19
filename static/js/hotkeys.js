7d93df2c3daae2fd9a554ef7b0791b2c545eac72
Altuğ Bakan <mail@alt.ug>
2025-11-19 13:28:03 +0100

    add vim hotkeys

--- static/js/hotkeys.js
+++ static/js/hotkeys.js
@@ -9,11 +9,13 @@ function focusInput(e) {
     input.focus();
 
     const len = input.value?.length || 0;
-    try { input.setSelectionRange(len, len); } catch (e) { }
+    try {
+      input.setSelectionRange(len, len);
+    } catch (e) {}
   }
 }
 
-document.addEventListener("keydown", async e => {
+document.addEventListener("keydown", async (e) => {
   if (e.target.tagName === "INPUT") return;
 
   switch (e.key) {
@@ -21,19 +23,39 @@ document.addEventListener("keydown", async e => {
       e.preventDefault();
       await sendToOverlay();
       break;
-    case "ArrowLeft": await navigateHistory(-1); break;
-    case "ArrowRight": await navigateHistory(1); break;
+    case "ArrowLeft":
+    case "h":
+    case "H":
+      await navigateHistory(-1);
+      break;
+    case "ArrowRight":
+    case "l":
+    case "L":
+      await navigateHistory(1);
+      break;
     case "f":
-    case "F": flipCard(); break;
+    case "F":
+      flipCard();
+      break;
     case "n":
     case "N":
+    case "k":
+    case "K":
       if (window.currentSet && window.currentNumber)
-        await fetchCardByNumber(window.currentSet, parseInt(window.currentNumber) + 1);
+        await fetchCardByNumber(
+          window.currentSet,
+          parseInt(window.currentNumber) + 1
+        );
       break;
     case "b":
     case "B":
+    case "j":
+    case "J":
       if (window.currentSet && window.currentNumber > 1)
-        await fetchCardByNumber(window.currentSet, parseInt(window.currentNumber) - 1);
+        await fetchCardByNumber(
+          window.currentSet,
+          parseInt(window.currentNumber) - 1
+        );
       break;
     case "/":
       focusInput(e);
